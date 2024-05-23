fbe62ea494f223e1d1dd084cf1cf629d1f98efde
kingpourya121 <hoseiniporya@gmail.com>
2024-05-23 16:20:17 +0330

    4

---
+++ s02/cw3.py
@@ -0,0 +1,4 @@
+str_1 = input('enter your first string: ')
+str_2 = input('enter your seccond string: ')
+
+print(str_2, str_1)
---
+++ s02/cw4.py
@@ -0,0 +1,10 @@
+num_1 = int(input('pleae enter first number: '))
+num_2 = int(input('pleae enter seccond number: '))
+
+sum_nums = num_1 + num_2
+mult = num_1 * num_2
+
+if mult >= 1000 :
+    print(sum_nums)
+else:
+    print(mult)
