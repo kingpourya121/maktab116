70dfbcf12b473e397faa0a66af053ae9be14ed0e
kingpourya121 <hoseiniporya@gmail.com>
2024-05-31 15:15:07 +0330

    cw4-2

--- s04/cw02.py
+++ s04/cw02.py
@@ -1,7 +1,12 @@
-my_list = [1,2,3,4,5,6,7,8,9]
+my_list = [11, 45, 8, 23, 14, 12, 78, 45, 89]
+
+
+
 n = int(input())
 chunk = int(len(my_list) / n)
 
 
 for i in range(0, len(my_list), chunk):
-    print(my_list[i:i+chunk])
+    new_list = my_list[i:i+chunk]
+    new_list.reverse()
+    print(new_list)
