99cf7984be06c70e08815d6709a409535934332a
kingpourya121 <hoseiniporya@gmail.com>
2024-05-31 15:22:03 +0330

    cw4-3

---
+++ s04/cw03.py
@@ -0,0 +1,9 @@
+speed = {'jan': 47, 'feb': 52, 'march': 47, 'April': 44, 'May': 52, 'June': 53, 'july': 54, 'Aug': 44, 'Sept': 54}
+
+speed_list = []
+
+for values in speed.values():
+    if values not in speed_list:
+        speed_list.append(values)
+
+print(speed_list)
